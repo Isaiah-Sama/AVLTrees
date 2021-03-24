9a350449dd19fba3f1d5fc9d70e195083462ce51
Isaiah-Sama <81326708+Isaiah-Sama@users.noreply.github.com>
2021-03-24 12:01:36 -0700

    Add files via upload

---
+++ AVL.java
@@ -0,0 +1,5 @@
+public class AVL {
+	private int height;
+	private AVL left, right, parent;
+	public AVLNode(O)
+}
---
+++ driver.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class driver {
+	public static void main(String args[]) {
+		int [] a = {1,2,3,4,5,6,7};
+		
+		
+	}
+	
+	public int findMin(){
+		return 0;
+	}
+	
+	public int insert() {
+		return 0;
+	}
+	
+	public int remove() {
+		return 0;
+	}
+	
+	public static void rotateLeft() {
+		
+	}
+	
+	public static void rotateRight() {
+		
+	}
+	
+	public static void doubleRotateRight() {
+		
+	}
+	
+	public static void doubleRotateLeft() {
+		
+	}
+}
